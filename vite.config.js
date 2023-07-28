98e062fbb8792d49016929dcc6985755e4319a4f
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2023-07-28 11:19:15 +0200

    Deploy again

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: 'minecraft-memory-game',
+    base: "",
+  };
+
+  if (command !== "serve") {
+    config.base = "";
   }
 
-  if (command !== 'serve') {
-    config.base = '/minecraft-memory-game/'
-  }
-
-  return config
-})
+  return config;
+});
